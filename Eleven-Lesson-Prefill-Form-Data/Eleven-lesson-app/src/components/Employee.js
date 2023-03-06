e8b4b63691553da66570cf18c0ef2957179996fd
Samuel Martín <alu0101359526@ull.edu.es>
2023-03-06 19:58:01 +0000

    Started the eleven lesson of the course

---
+++ Eleven-Lesson-Prefill-Form-Data/Eleven-lesson-app/src/components/Employee.js
@@ -0,0 +1,30 @@
+import EditEmployee from "./EditEmployee";
+
+/* eslint-disable jsx-a11y/alt-text */
+function Employee(props) {
+    return (
+        <div class="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+            {/* Nótese que se hace uso de las sentencias como object cover o rounded-full para darle a las imágenes de perfil una apariencia 
+            de redondeo a las imágenes que se encuentran dentro.*/}
+            <img 
+            class="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" 
+            src={props.img} 
+            />
+            <div class="text-center space-y-2 sm:text-left">
+                <div class="space-y-0.5">
+                    <p class="text-lg text-black font-semibold">
+                        {props.name}
+                    </p>
+                    <p class="text-slate-500 font-medium">
+                        {props.role}
+                    </p>
+                </div>
+
+                <EditEmployee />
+
+            </div>
+        </div>
+    );
+}
+
+export default Employee;
