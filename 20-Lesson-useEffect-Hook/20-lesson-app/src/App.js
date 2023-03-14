6c1c3e1bd1f89d52dbf253cb0393e49bff8f5f00
Samuel Martín <alu0101359526@ull.edu.es>
2023-03-14 19:42:24 +0000

    Started the 20 lesson of the course

---
+++ 20-Lesson-useEffect-Hook/20-lesson-app/src/App.js
@@ -0,0 +1,26 @@
+import "./index.css";
+import Headers from "./components/Header";
+import Employees from "./pages/Employees";
+import Customers from "./pages/Customers";
+// Esto de aquí son los componentes que se van a usar en la aplicación,
+// en este caso se va a usar el componente Header y el componente Employees,
+// el componente Header es el que va a contener las rutas de la aplicación,
+// y el componente Employees es el que va a contener la lista de empleados.
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    // Hay que tener en cuenta que no se puede cerrar el header con un <Header />, ya que este componente se
+    // va a usar como un contendor de páginas para este caso
+    <BrowserRouter>
+      <Headers>
+        <Routes>
+          <Route path="/employees" element={<Employees />} />
+          <Route path="/customers" element={<Customers />} />
+        </Routes>
+      </Headers>
+    </BrowserRouter>
+  );
+}
+
+export default App;
