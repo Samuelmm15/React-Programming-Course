efe274f24577aea1bcae6e562b0e9b0032e6d348
Samuelmm15 <alu0101359526@ull.edu.es>
2023-04-06 17:00:16 +0100

    Finished the 32 lesson of the course

--- 32-Lesson-Create-Details-Page/32-lesson-app/README.md
+++ 32-Lesson-Create-Details-Page/32-lesson-app/README.md
@@ -1,9 +1,5 @@
-# 31 lesson Notes
+# 32 lesson Notes
 
-At this lesson, we are going to create details into the different array of the backend. We will create a detail using the ID of the different objects that has the database created at the backed of the application.
+At this lesson, we are going to create a detail page to display the information of the backend data.
 
-And, at this lesson, we are going to create a new component to show the details of the different objects, and we are going to add a real time component that permits to edit the different objects. To do this, we need to use POST and GET operations to the backend.
-
-## **NOTE**
-
-At this lesson, we are going to edit the backend files.
+Now, we are gonna to write and create the file `Customer.js`, that will be used to display the information of the backend data.
--- 32-Lesson-Create-Details-Page/32-lesson-app/src/App.js
+++ 32-Lesson-Create-Details-Page/32-lesson-app/src/App.js
@@ -4,34 +4,25 @@ import Employees from "./pages/Employees";
 import Customers from "./pages/Customers";
 import Dictionary from "./pages/Dictionary";
 import NotFound from "./components/NotFound";
-// Esto de aquí son los componentes que se van a usar en la aplicación,
-// en este caso se va a usar el componente Header y el componente Employees,
-// el componente Header es el que va a contener las rutas de la aplicación,
-// y el componente Employees es el que va a contener la lista de empleados.
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Definition from "./pages/Definition";
+import Customer from "./pages/Customer";
 
 function App() {
   return (
-    // Hay que tener en cuenta que no se puede cerrar el header con un <Header />, ya que este componente se
-    // va a usar como un contendor de páginas para este caso
+   
     <BrowserRouter>
       <Headers>
         <Routes>
           <Route path="/employees" element={<Employees />} />
           <Route path="/dictionary" element={<Dictionary />} />
-          {/* Hay que tener en cuenta que lo que se tiene abajo :search, hace
-          referencia al empleo de parametros o variables dentro de las rutas de
-          busqueda que, para ente caso se emplea como un elemento para comprobar en nuesta API de diccionario*/}
           <Route path="/dictionary/:search" element={<Definition />} />
           <Route path="/customers" element={<Customers />} />
-          {/* Hay que tener en cuenta que el componente NotFound, es el que se va a usar para
-          mostrar la pagina de error 404, y para que esto funcione, se debe de hacer la
-          configuracion de la ruta de la pagina de error 404 */}
+          {/* Hay que tener en cuenta que esta es la nueva ruta que se va a añadir para el episodio 32
+          ya que, se va a mostrar la información detallada de un usuario en una página nueva que viene dada
+          por la identificación de cada uno de los objetos que se encuentran dentro del backend */}
+          <Route path="/customers/:id" element={<Customer />} />
           <Route path="/404" element={<NotFound />} />
-          {/* Esta ruta que se puede observar a continuacion es la ruta por defecto, y,
-          se usa para poder establecer cual es la pagina que por defecto te va a mostrar tu sitio
-          web, en el caso de que se produzca algun fallo */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Headers>
