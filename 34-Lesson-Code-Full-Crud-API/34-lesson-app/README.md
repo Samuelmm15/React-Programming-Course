fe92a14395a44a48b4fee8de90965b647b501283
Samuelmm15 <alu0101359526@ull.edu.es>
2023-04-07 12:51:06 +0100

    Finished the 34 lesson of the course

--- 34-Lesson-Code-Full-Crud-API/34-lesson-app/README.md
+++ 34-Lesson-Code-Full-Crud-API/34-lesson-app/README.md
@@ -1,11 +1,3 @@
 # 32 lesson Notes
 
-At this lesson, we are going to create a 404 page that will be displayed when a user tries to access a page that does not exist.
-
-## **NOTE**
-
-At this time we are going to display the 404 page at the backen of the application, not at the frontend of the application.
-
-To take a 404 error, we need to code the try-except block at the backed because, if we are going to add this error at the backend, first we need to create this error and coded at the backed of the application.
-
-At the final of the lesson, we create a general JavaScript file that is called `shared.js` and it contains the different urls that we are going to use in the application.
+At this lesson, we are going to edit the backend, and create a real CRUD application at the backend, so, at this lesson we are going to update the backen and perfection that.
--- backend/customers/views.py
+++ backend/customers/views.py
@@ -1,20 +1,53 @@
 from customers.models import Customer
 from django.http import JsonResponse, Http404
 from customers.serializers import CustomerSerializer
+from rest_framework.decorators import api_view
+# Esto sirve para poder administrar todas las distintas respuestas que se pueden dar a un cliente.
+from rest_framework.response import Response
+# Esto sirve para poder administrar los distintos estados que puede tener una respuesta.
+from rest_framework import status
 
+@api_view(['GET', 'POST'])
 def customers(request):
-  #invoke serealizer and return to client
-  data = Customer.objects.all()
-  serializer = CustomerSerializer(data, many=True)
-  return JsonResponse({'customers': serializer.data})
+  # En este punto realizamos la obtención de la lista total de clientes que se encuentran en la base de datos
+  if request.method == 'GET':
+    data = Customer.objects.all()
+    serializer = CustomerSerializer(data, many=True)
+    return Response({'customers': serializer.data}, status=status.HTTP_200_OK)
+    # En este punto se realiza la creación de un nuevo cliente en la base de datos.
+  elif request.method == 'POST':
+    serializer = CustomerSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      # HAY QUE TENER EN CUENTA QUE CADA VEZ QUE SE PONE EL SERIALIZER.DATA, LO QUE PONEMOS DELANTE ES UNA KEY QUE SE USA PARA EL DICCIONARIO DE LA BASE DE DATOS
+      return Response({'customers': serializer.data}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+# Para poder entender que es @, esto en python se conoce como decorador, y es una función que recibe como parámetro otra función.
+# y sirve para poder agregar funcionalidades a una función, en este caso, se le agrega la funcionalidad de poder recibir
+# los métodos GET, POST y DELETE.
+@api_view(['GET', 'POST', 'DELETE'])
 def customer(request, id):
-  # Para poder obtener un error del tipo 404 procedente del backend de la aplicación, es necesario
-  # generar este tipo de error y lanzarlo, porque si no lo hacemos se van a lanzar errores del tipo 500 y no el que estamos buscando en concreto.
   try:
     data = Customer.objects.get(pk=id)
   except Customer.DoesNotExist:
-    raise Http404("Customer does not exist")
-  # TENER EN CUENTA ESTAS CARACTERÍSTICAS ANTERIORES PARA PODER REALIZAR ESTO DE MANERA CORRECTA
-  serializer = CustomerSerializer(data)
-  return JsonResponse({'customer': serializer.data})
+    # Cabe desatacar que al hacer uso del método `Response` se produce una mejor interfaz de usuario en la administración y en el cliente, permitiendo establecer que este método es mejor que el anteriormente usado.
+    return Response(status=status.HTTP_404_NOT_FOUND)
+
+  # Para este punto se va a realizar la implementación de todos los métodos especificados anteriormente
+  if request.method == 'GET':
+    serializer = CustomerSerializer(data)
+    # Hay que tener en cuenta que el método `Response` permite establecer varias opciones en vez de usar el método `JsonResponse`
+    return Response({'customer': serializer.data}, status=status.HTTP_200_OK)
+  elif request.method == 'DELETE':
+    # Este método sirve para eliminar el dato que ha sido obtenido mediante el GET realizado anteriormente.
+    data.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+  elif request.method == 'POST':
+    # Este método u operación sirve para actualizar los datos de un cliente en particular.
+    serializer = CustomerSerializer(data, data=request.data)
+    # En este punto debemos de comprobar si los datos actualizados son válidos.
+    if serializer.is_valid():
+        serializer.save()
+        return Response({'customer': serializer.data}, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
