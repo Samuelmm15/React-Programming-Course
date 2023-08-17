8d38876157f15c3ce109665abe5cd1ee5618d028
Samuel Martín <alu0101359526@ull.edu.es>
2023-08-17 12:55:09 +0100

    Finished the lesson 56 of the React Course

--- 56-Lesson-Generate-Drop-Down-List/56-lesson-app-typescript/README.md
+++ 56-Lesson-Generate-Drop-Down-List/56-lesson-app-typescript/README.md
@@ -1,24 +1,7 @@
-# 55 Lesson
+# 56 Lesson
 
-At this lesson we are going to add components to our app using typescript.
+At this lesson we are going to create a dropdown list using typescript and an API.
 
 At this lesson, we are going to edit the files called `App.tsx`
-
-`IMPORTANT`:
-we can see the important information to use typeScript and React, we can see it into the next website:
-[typescript-cheatsheet](https://react-typescript-cheatsheet.netlify.app).
-
-## `useState` using TypeScript
-
-When we use useState using typeScript the style of using this is similar to the example that you can see
-below:
-
-```typescript
-import {useState} from "react";
-
-const [example, setExample] = useState<Example | null>(null);
-```
-
-So now, we are going to use `Function components` at this 55 lesson of the React Course.
 
 
--- 56-Lesson-Generate-Drop-Down-List/56-lesson-app-typescript/src/App.tsx
+++ 56-Lesson-Generate-Drop-Down-List/56-lesson-app-typescript/src/App.tsx
@@ -5,34 +5,48 @@ import CryptoSummary from "./components/CryptoSummary";
 import {Crypto} from "./Types";
 
 function App() {
-    /*
-        Nótese que el uso y empleo de dicha manera del useState hace referencia a que si el State contiene
-        algo contiene un array de elementos que sigue la estructura definida anteriormente, en otro caso dicha variable
-        es nula.
-     */
-    const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
+  /*
+      Nótese que el uso y empleo de dicha manera del useState hace referencia a que si el State contiene
+      algo contiene un array de elementos que sigue la estructura definida anteriormente, en otro caso dicha variable
+      es nula.
+   */
+  const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
+  const [selected, setSelected] = useState<Crypto | null>();
 
-    useEffect(() => {
-        const url =
-          'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en';
-        // En este punto vamos a hacer uso de lo nuevo de axios. Cabe destacar que este es muy similar a un fetch
-        axios.get(url).then((response) => {
-            setCryptos(response.data);
-        })
-    }, []);
-    return (
+  useEffect(() => {
+    const url =
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en';
+    // En este punto vamos a hacer uso de lo nuevo de axios. Cabe destacar que este es muy similar a un fetch
+    axios.get(url).then((response) => {
+      setCryptos(response.data);
+    })
+  }, []);
+  return (
+    <>
       <div className="App">
+        {/*
+      Haciendo uso del select and option en HTML podemos crear de manera predeterminada un menú desplegable
+      dónde, para este caso, cada uno de los distintas opciones que se encuentran dentro de dicho menú desplegable
+      pues se establecen mediante el option que se encuentra dentro del select.
+      */}
+        <select onChange={(e) => {
+          const c = cryptos?.find((x) => x.id === e.target.value);
+          setSelected(c);
+        }} defaultValue = "default">
           {/*
-                De la manera que podemos observar a continuación, funcionan los maps dentro de typscript, es decir
-                dentro de dichos métodos se debe de hacer uso de un callback que nos permita imprimir en HTML
-                los distintos elementos del array que se ha creado y que se ha establecido el tipo de datos de dicho
-                array anteriormente como se ha podido observar.
-            */}
+          De la manera que podemos observar a continuación y en la línea anterior, de dicha manera es como se
+          establece alguna de las opciones del menú desplegable como la opción por defecto.
+          */}
+          <option value="default">Choose an option...</option>
           {cryptos ? cryptos.map((crypto) => {
-              return <CryptoSummary crypto={crypto}/>
+            return <option key={crypto.id} value={crypto.id}>{crypto.name}</option>
           }) : null}
+        </select>
       </div>
-    );
+      {selected ? <CryptoSummary crypto={selected}/> : null}
+    </>
+  )
+    ;
 }
 
 export default App;
