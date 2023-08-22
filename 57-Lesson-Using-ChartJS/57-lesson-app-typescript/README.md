a6e55b66be87a2e17954cc326bf10c0ee417416f
Samuel Martín <alu0101359526@ull.edu.es>
2023-08-22 22:56:22 +0100

    Finished the lesson 57 of the React Course

--- 57-Lesson-Using-ChartJS/57-lesson-app-typescript/README.md
+++ 57-Lesson-Using-ChartJS/57-lesson-app-typescript/README.md
@@ -1,7 +1,28 @@
-# 56 Lesson
+# 57 Lesson
 
-At this lesson we are going to create a dropdown list using typescript and an API.
+At this lesson we are going to create a graph that the user can see the progress of the crypto coins, so
+we need to create the granph and a dropdown that can give to the user to select an option to show to the
+user.
 
 At this lesson, we are going to edit the files called `App.tsx`
 
 
+## Installation of Chart.js
+
+At first, we need to install the package at npm called Chart.js. So the command is:
+
+```bash
+npm install chart.js react-chartjs-2
+```
+
+We can see some examples of the use of this package at the website at this [link](https://react-chartjs-2.js.org/examples)
+
+## Installation of Moment
+
+This library contains the different implementation of the dropdown menu that changes the time of the chart.
+
+To install moment:
+
+```bash
+npm install moment
+```
--- 57-Lesson-Using-ChartJS/57-lesson-app-typescript/src/App.tsx
+++ 57-Lesson-Using-ChartJS/57-lesson-app-typescript/src/App.tsx
@@ -3,6 +3,31 @@ import axios from 'axios';
 import {useEffect, useState} from 'react';
 import CryptoSummary from "./components/CryptoSummary";
 import {Crypto} from "./Types";
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend, ChartType,
+} from 'chart.js';
+import {Line} from 'react-chartjs-2';
+import type {ChartData, ChartOptions} from 'chart.js';
+// This is the new library called moment
+import moment from 'moment';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 function App() {
   /*
@@ -12,6 +37,21 @@ function App() {
    */
   const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
   const [selected, setSelected] = useState<Crypto | null>();
+  // Realizamos la creación de dos nuevas variables que nos permitan tener en cuenta los datos y las opciones de las gráficas
+  // creadas mediante Chart.js
+  const [data, setData] = useState<ChartData<'line'>>();
+  const [options, setOptions] = useState<ChartOptions<'line'>>({
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart',
+      },
+    },
+  });
 
   useEffect(() => {
     const url =
@@ -32,7 +72,28 @@ function App() {
         <select onChange={(e) => {
           const c = cryptos?.find((x) => x.id === e.target.value);
           setSelected(c);
-        }} defaultValue = "default">
+          // request
+          axios.get(`https://api.coingecko.com/api/v3/coins/${c?.id}/market_chart?vs_currency=usd&days=30&interval=daily`)
+            .then((response) => {
+              setData({
+                labels: response.data.prices.map((price: number[]) => {
+                  // Esto lo que nos permite es cambiar el tiempo de la gráfica haciendo uso de la librería moments.
+                  return moment.unix(price[0] / 1000).format('MM-DD');
+                }),
+                datasets: [
+                  {
+                    label: 'Dataset 1',
+                    data: response.data.prices.map((price: number[]) => {
+                      return price[1];
+                    }),
+                    borderColor: 'rgb(255, 99, 132)',
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                  },
+                ],
+              });
+            })
+          // update data state
+        }} defaultValue="default">
           {/*
           De la manera que podemos observar a continuación y en la línea anterior, de dicha manera es como se
           establece alguna de las opciones del menú desplegable como la opción por defecto.
@@ -44,6 +105,11 @@ function App() {
         </select>
       </div>
       {selected ? <CryptoSummary crypto={selected}/> : null}
+      {/*
+      En este punto realizar el renderizado de la gráfica creada de la siguiente manera que se puede observar a
+      continuación.
+      */}
+      {data ? <div style={{width: 600}}><Line options={options} data={data}/></div> : null}
     </>
   )
     ;
